d18a4919b8fe712d9d90e7852d18d3d64f4d2be5
irakojf <irakojf@gmail.com>
2022-09-15 21:57:08 -0400

    Add v1 of static webpage

---
+++ script/index-script.js
@@ -0,0 +1,36 @@
+async function submitPromoCode(purchase_id) {
+    let api = "https://8vc8x8ak9k.execute-api.us-east-1.amazonaws.com/v1/cars?id=" + purchase_id;
+
+    fetch(api)
+    .then( 
+        async (response) => {
+            // get json response here            
+            let data = await response.json();
+            if (response.status === 200) {
+                console.log(response.status);
+                location.assign(data);
+            } else {
+                console.log(response.status);
+                console.log("Incorrect purchase ID");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    
+
+function listenFormSubmit() {
+    // use regex to filter out for numbers only
+    var numberPattern = /\d+/g;
+    var purchase_id = document.getElementById('promo').value;
+    purchase_id = purchase_id.match(numberPattern);
+    submitPromoCode(purchase_id);
+}
+
+document.getElementById('promo').addEventListener('keydown', function(event) { 
+    if (event.code === 'Enter') {
+        console.log('listening');
+        listenFormSubmit();     
+    }
+})
---
+++ script/script.js
@@ -0,0 +1,56 @@
+document.getElementById("venmoNoti").addEventListener("click", venmoNotification);
+
+function venmoNotification (url) { 
+    let accountName = document.getElementById("accountName").textContent;
+    let amount = document.getElementById("fiatValue").textContent;
+    Email.send({
+        SecureToken : "8606ec61-1080-4b9a-8b96-12a46d9c9b51",
+        To : 'irakojf@gmail.com',
+        From : "hautewheels@proton.me",
+        Subject : "Hautewheels Venmo Request - " + accountName,
+        Body : accountName + " has triggered a Venmo request for " + amount + " ."
+    }).then(
+    message => alert("Ira will text you shortly :x")
+    );
+    reload();
+}
+
+document.getElementById("cryptoNoti").addEventListener("click", venmoNotification);
+
+function cryptoNotification (url) { 
+    let accountName = document.getElementById("accountName").textContent;
+    let amount = document.getElementById("ethValue").textContent;
+    Email.send({
+        SecureToken : "8606ec61-1080-4b9a-8b96-12a46d9c9b51",
+        To : 'irakojf@gmail.com',
+        From : "hautewheels@proton.me",
+        Subject : "Hautewheels Crypto Request - " + accountName,
+        Body : accountName + " has triggered a Crypto request for " + amount + " ."
+    }).then(
+    message => alert("Ira will text you shortly :x")
+    );
+    reload();
+}
+
+async function cryptoPriceChecker () { 
+    // grab the amount of ethereum
+    let eth = document.getElementById("ethValue").textContent;
+
+    let key = "b356bf0263af5f62f752053ff71c82deab00b48c17d7b7afcd65615968aa4a3a";
+    let url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD&api_key=" + key; 
+
+    // fetch the price of ethereum in the form of a JSON object
+    let response = await fetch(url); 
+    const responseText = await response.json(); 
+
+    // multiply ETH by ETH price in USD
+    fiatValue = responseText['ETH']['USD'] * eth;
+
+    // format in USD
+    fiatValueFormatted = "$" + Intl.NumberFormat('en-US').format(fiatValue);
+
+    // update the innertext 
+    document.getElementById("fiatValue").innerText = fiatValueFormatted;
+}
+
+cryptoPriceChecker();
